48e530f69eb274c4d5adab73140b0502a3d13e9d
Sarabot2010 <99358625+Sarabot2010@users.noreply.github.com>
2022-09-17 12:36:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+nosex=0;
+nosey=0
+
 function preload(){
-    
+    filtro = loadImage('https://i.postimg.cc/tTHTSxGJ/pngwing-com-3.png');
 }
 
 function setup() {
@@ -13,6 +16,7 @@ function setup() {
 }
 function draw(){
     image(video,0,0,300,300);
+    image(filtro,nosex,nosey,150,150);
 }
 
 function take_snapshot(){
@@ -20,4 +24,13 @@ function take_snapshot(){
 }
 function modelLoaded(){
     console.log('PoseNet se ha cargado');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        nosex = results[0].pose.nose.x-80;
+        nosey = results[0].pose.nose.y-105;
+        console.log("nose x= "+results[0].pose.nose.x);
+        console.log("nose y= "+results[0].pose.nose.y);        
+    }
 }
